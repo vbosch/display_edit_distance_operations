65b86b5227821ce44fca4fd2d0e3123c85c782fe
vbosch <vbosch@gmail.com>
2011-10-18 15:37:09 +0200

    added frontier functionality

--- lib/display_edit_distance_operations/distance_operations_file.rb
+++ lib/display_edit_distance_operations/distance_operations_file.rb
@@ -1,6 +1,6 @@
 module DisplayEditDistanceOperations
 
-  class Operation
+  class LineOperation
 
     attr_reader :type
 
@@ -13,23 +13,50 @@ module DisplayEditDistanceOperations
 
     def to_a(width)
       return [0,@original[0] ,0,@original[1] ,width-1,@original[1], width-1,@original[0]] if @type == :insertion
-      return [0,@original[1] ,width-1,@hypothesis[1] ,width-1,@hypothesis[0] ,0,@original[1]] if @type == :deletion
+      return [0,@hypothesis[1] ,width-1,@hypothesis[1] ,width-1,@hypothesis[0] ,0,@hypothesis[0]] if @type == :deletion
       [0,@original[0] ,0,@original[1],width-1,@hypothesis[1] ,width-1,@hypothesis[0]]
+    end
+  end
+
+  class FrontierOperation
+
+    attr_reader :type
+
+    def initialize(values)
+
+      @type = values[0].to_sym
+      @hypothesis = values[1].to_i
+      @original = values[2].to_i
+    end
+
+    def to_a(width)
+      return [0,@original,width-1,@original] if @type == :insertion
+      return [0,@hypothesis,width-1,@hypothesis] if @type == :deletion
+      [0,@original,width-1,@hypothesis]
     end
   end
 
   class DistanceOperationsFile
 
     attr_reader :operations
-    def initialize(file_name)
+    def initialize(file_name,op_type)
       @file_name = file_name
       @operations = []
+      case op_type
+      when "frontier" then
+          @op_rep= Object.const_get("DisplayEditDistanceOperations").const_get("FrontierOperation")
+      when "line" then
+          @op_rep= Object.const_get("DisplayEditDistanceOperations").const_get("LineOperation")
+      else
+        raise "Unimplemented operation type passed"
+      end
+
     end
 
     def read
       File.open(@file_name,"r") do |file|
         while (line = file.gets)
-          @operations.push(Operation.new(line.split))
+          @operations.push(@op_rep.new(line.split))
         end
       end
     end
--- lib/display_edit_distance_operations/image.rb
+++ lib/display_edit_distance_operations/image.rb
@@ -2,6 +2,7 @@ module DisplayEditDistanceOperations
 
   require 'RMagick'
   require 'ruby-debug'
+  require 'ap'
 
   class Image
     def initialize(ex_path)
@@ -10,14 +11,22 @@ module DisplayEditDistanceOperations
       @colors={:deletion => "red", :substitution => "seagreen", :insertion => "steelblue"}
     end
 
-    def draw_edit_operations(operations_file, output_file)
+    def draw_edit_operations(operations_file,operation_mode, output_file)
 
+      draw_operation = "line"
+      draw_operation = "polygon" if operation_mode == "line"
       gc = Magick::Draw.new
-      gc.stroke_width = 2
+      gc.stroke_width(3)
       operations_file.operations.each do |operation|
-        gc.fill(@colors[operation.type])
+
+        if draw_operation == "polygon"
+          gc.fill(@colors[operation.type])
+        else
+          gc.stroke(@colors[operation.type])
+        end
+
         gc.fill_opacity(0.5)
-        gc.polygon(*operation.to_a(@img.columns))
+        gc.send(draw_operation,*operation.to_a(@img.columns))
       end
 
       gc.draw(@img)
